2fbbeccc559eea9182f1287582168cf22aad0afe
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2025-10-18 09:01:34 +0530

    cng

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -194,7 +194,7 @@ export const loginUser = async (req, res) => {
     if (!isMatch) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
     res.status(200).json({ token });
   } catch (err) {
     res.status(500).json({ message: err.message });
