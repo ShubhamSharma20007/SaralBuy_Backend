02a5e5383f6b214382dda6f52f8c5ccd525a2c20
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2025-11-20 22:26:20 +0530

    cng

--- server.js
+++ server.js
@@ -12,13 +12,16 @@ import chatHandler from './chatHandler/index.js';
 const app = express()
 const server = http.createServer(app);
 mongoCtx()
-app.use(cors({
-  origin: ['http://localhost:5173','http://localhost:5174','https://kaleidoscopic-pika-c2b489.netlify.app','https://saralbuy.com','https://curious-khapse-f12cd1.netlify.app'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-  exposedHeaders: ['Set-Cookie']
-}));
+// app.use(cors({
+//   origin: ['http://localhost:5173','http://localhost:5174','https://kaleidoscopic-pika-c2b489.netlify.app','https://saralbuy.com','https://curious-khapse-f12cd1.netlify.app'],
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+//   exposedHeaders: ['Set-Cookie']
+// }));
+
+app.use(cors())
+
 app.use(cookieParser());
 app.use(express.json({limit:'10mb'}))
 app.use(express.urlencoded({ extended: true }))
