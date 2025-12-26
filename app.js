fad912f84d6864367d24ed99189a74cd034a20d2
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2025-12-26 19:45:11 +0530

    cng

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use('/api/v1',router)
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 
--- build/assets/currencyConvertor-IA5kyJ7b.js
+++
@@ -1,1 +0,0 @@
-const n=(r,t="INR")=>new Intl.NumberFormat("en-IN",{style:"currency",currency:t,minimumFractionDigits:0}).format(r);export{n as c};
