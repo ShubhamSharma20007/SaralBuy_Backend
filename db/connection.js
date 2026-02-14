cead7f61d76d9cbbf9589a3ac6b279cad010726d
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2026-02-14 10:08:39 +0530

    add bugs

--- db/connection.js
+++ db/connection.js
@@ -10,6 +10,7 @@ export default function mongoCtx(){
     })
     .then(() => {
         console.log("Connected to MongoDB 🚀");
+        throw new Error('NEW ERROR Added')
     })
     .catch((error) => {
         console.error("Error connecting to MongoDB", error);
